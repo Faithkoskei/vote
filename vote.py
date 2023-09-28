5ae663ee60ac4632bc4a9ccb9361db33ba4d4f52
Faithkoskei <92671391+Faithkoskei@users.noreply.github.com>
2023-09-28 12:52:58 +0300

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,7 @@
+#Eligible to vote
+citizenship=input("Enter citizenship")
+age=int(input("Enter age"))
+if citizenship in"kenyan" "Uganda" "Tanzania"and age>=18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")
